a685a4783d8673577b06ed7390b9f44ba027f968
Gunn <gunn@Gunns-MacBook-Pro.local>
2017-05-04 14:10:22 +0700

    first commit

---
+++ src/sqltest2/SqlTest2.java
@@ -0,0 +1,55 @@
+package sqltest2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import javax.swing.*;
+
+
+public class SqlTest2 extends BasicVariable{
+    public static void main(String[] args) throws SQLException {
+        BasicVariable link = new BasicVariable();
+        Scanner in = new Scanner(System.in);
+        
+     
+//        
+        JFrame login = new loginFrame();
+        
+        login.setVisible(true);
+//        JFrame edit = new editMenuFrame();
+//        edit.setVisible(true);
+        
+        
+//        String query = "select* from Manu";
+//        st = conn.createStatement();
+//        int employeeID=0;
+//        String password="";
+//        String username="";
+//        System.out.println("Username: ");
+//        username = in.next();
+//        rs=st.executeQuery("SELECT employeeID,password FROM Employee WHERE Username ="+"\'"+username+"\'");
+//        try{
+//         rs.next();
+//         employeeID=rs.getInt("employeeID");
+//         password = rs.getString("password");
+//        }
+//        catch(SQLException e){
+//                if(e.getErrorCode()==0){
+//                    System.out.println("Don't have this Username");
+//                    
+//                }
+//            }
+//        System.out.println(employeeID+password);
+//    }
+}
+}
+
+    
+
+
+    
+
